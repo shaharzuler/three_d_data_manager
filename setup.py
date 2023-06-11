7cdf8ab5bd317dedfd758e8818a8a40b9a87d968
shahar.zuler <shaharzuler@gmail.com>
2023-06-11 14:39:04 +0300

    adding requirements to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+with open('requirements.txt') as f:
+    requirements = [str(req) for req in parse_requirements(f)]
+
 
 setup(
     name='three_d_data_manager',
     version='0.1.0',
     packages=find_packages(),
-
+    install_requires=requirements,
     author='Shahar Zuler',
     author_email='shahar.zuler@gmail.com',
     description='A package for managing 3D data',
