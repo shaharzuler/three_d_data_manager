690f37c98a48763f1e9136b1fc268757b66ff2e4
shahar.zuler <shaharzuler@gmail.com>
2023-07-13 14:41:59 +0300

    json savingindentation

--- three_d_data_manager/src/utils/os_utils.py
+++ three_d_data_manager/src/utils/os_utils.py
@@ -8,4 +8,4 @@ def write_config_file(folder_path:str, file_prefix:str, content:dict):
         except TypeError:
             content[k] = str(v)
     with open(os.path.join(folder_path, f"{file_prefix}_config.json"), "w") as f:
-        f.write(json.dumps(content) )
+        f.write(json.dumps(content, indent=4) )
