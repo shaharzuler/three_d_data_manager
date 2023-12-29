70110fa68f78be5d51066a0dbe5f0aaec7a24202
shaharzuler <shahar.zuler@gmail.com>
2023-12-29 21:05:47 +0200

    saving the normals ans np instead of ply

--- three_d_data_manager/src/data_creators/vertex_normals_data_creator.py
+++ three_d_data_manager/src/data_creators/vertex_normals_data_creator.py
@@ -18,7 +18,7 @@ class VertexNormalsDataCreator(DataCreatorBase): # TODO add to readme and exampl
 
     def check_if_exists_default_filename(self) -> bool:
         self.filename = f"{self.default_filename}_from_{self.creation_args.orig_geometry_name}"
-        self.normals_path = os.path.join(self.subject_dir, f"{self.filename}.ply") 
+        self.normals_path = os.path.join(self.subject_dir, f"{self.filename}.npy") 
         return os.path.isfile(self.normals_path)
 
     def add_sample(self, target_root_dir:str, file_paths:FilePaths, dataset_attrs:Dict[str,str]=None) -> FilePaths:
@@ -28,11 +28,9 @@ class VertexNormalsDataCreator(DataCreatorBase): # TODO add to readme and exampl
                 geometry = o3d.io.read_triangle_mesh(self.creation_args.geometry_path) # TODO add option for point cloud maybe try catch
                 geometry.compute_vertex_normals(normalized=True)
                 normals = np.asarray(geometry.vertex_normals)
-                point_cloud_scruct = o3d.geometry.PointCloud()
-                point_cloud_scruct.points = o3d.utility.Vector3dVector(normals)
             else:
-                normals =  o3d.io.read_point_cloud(self.normals_path)
-            o3d.io.write_point_cloud(self.normals_path, point_cloud_scruct) # taking advantage of pointcloud data structure for saving this normals file.
+                normals =  np.asarray(o3d.io.read_point_cloud(self.normals_path))
+            np.save(self.normals_path, normals)
             if self.creation_args is not None:
                 os_utils.write_config_file(self.subject_dir, self.filename, asdict(self.creation_args))
 
