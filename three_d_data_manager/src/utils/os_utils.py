7ad4a2f49152c0df21a8e341d9c87a0fb4bd7cc1
shaharzuler <shahar.zuler@gmail.com>
2024-02-14 12:43:05 +0200

    added read filepaths to os_utils

--- three_d_data_manager/src/utils/os_utils.py
+++ three_d_data_manager/src/utils/os_utils.py
@@ -9,3 +9,12 @@ def write_config_file(folder_path:str, file_prefix:str, content:dict):
             content[k] = str(v)
     with open(os.path.join(folder_path, f"{file_prefix}_config.json"), "w") as f:
         f.write(json.dumps(content, indent=4) )
+
+def read_filepaths(dataset_root_path):
+    possible_path = os.path.join(dataset_root_path, "file_paths.json")
+    if os.path.isfile(possible_path):
+        with open(possible_path, 'r') as f:
+            j_dict = json.load(f)
+            return j_dict
+    else:
+        return None
